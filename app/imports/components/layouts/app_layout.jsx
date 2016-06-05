525e531140844b7905e22b644074585bc84c1b8c
Anders Ramsay <andersr@gmail.com>
2016-06-05 11:40:50 -0400

    add app-container to homepage

--- app/imports/components/layouts/app_layout.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const AppLayout = (props) => <div>{props.content(props)}</div>
--- app/imports/components/pages/homepage.jsx
+++ app/imports/components/pages/homepage.jsx
@@ -1,3 +1,10 @@
 import React from 'react'
 
-export const Homepage = (props) => <div>{"Homepage content goes here"}</div>
+export const Homepage = (props) =>
+  <div id="app-container">
+     {"App Header will go here"}
+    <div id="main-content">
+      {"Homepage content goes here"}
+    </div>
+  </div>
+
