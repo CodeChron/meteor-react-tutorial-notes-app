f5eb187ba2f6c149761f464bdcc94fe4d8c2f189
Anders Ramsay <andersr@gmail.com>
2016-06-05 13:34:16 -0400

    apply SMACCS css structure

--- app/imports/components/pages/homepage.jsx
+++ app/imports/components/pages/homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const Homepage = (props) =>
-  <div id="app-container">
+  <div id="app-container" className="l-app-full-height l-app-centered">
      {"App Header will go here"}
     <div id="main-content">
       {"Homepage content goes here"}
