b60a3face17385105136074dcf1651390d8dda8d
Anders Ramsay <andersr@gmail.com>
2016-06-05 13:57:56 -0400

    add app header

--- app/imports/components/layouts/app_header.jsx
+++ app/imports/components/layouts/app_header.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export const AppHeader = (props) => {
 
-	return  <div className="flex-row-centered">
+	return  <div className="l-flex-row-centered">
 	          <div className="flex-left-right-icons">{props.leftCol}</div>
-	          <div classNbame="flex-main-content">{props.middleCol}</div>
+	          <div classNbame="l-flex-main-content">{props.middleCol}</div>
 	          <div className="flex-left-right-icons">{props.rightCol}</div>
 	        </div>
 }
 
 AppHeader.propTypes = {
   leftCol: React.PropTypes.object,
-  middleCol: React.PropTypes.object,
+  middleCol: React.PropTypes.object.isRequired,
   rightCol: React.PropTypes.object
 }
