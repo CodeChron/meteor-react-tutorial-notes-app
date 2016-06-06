3a16671dcce9b563fd0b24ab650351d70b12a36f
Anders Ramsay <andersr@gmail.com>
2016-06-06 11:13:55 -0400

    note details pub

--- app/imports/collections/server/publications.js
+++ app/imports/collections/server/publications.js
@@ -5,8 +5,16 @@ const
   notesListFields = {
     title: 1,
     updatedAt: 1
-  }
+  },
+  noteDetailsFields = {
+    title: 1,
+    content: 1
+  } 
 
 Meteor.publish('notes.list', function() {
   return Note.find({}, { fields: notesListFields})
+})
+
+Meteor.publish('note.details', function(id) {
+  return Note.find({_id: id }, { fields: noteDetailsFields})
 })
