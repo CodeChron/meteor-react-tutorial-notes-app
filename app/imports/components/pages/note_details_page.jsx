451c336de96ac6bdd6d13633a74c44d236c8ce7c
Anders Ramsay <andersr@gmail.com>
2016-06-06 14:18:21 -0400

    add back btn to note details page

--- app/imports/components/pages/note_details_page.jsx
+++ app/imports/components/pages/note_details_page.jsx
@@ -3,15 +3,22 @@ import { AppHeader } from '../layouts/app_header'
 import { PageTitle } from '../content/page_title'
 import { EditableContent } from '../content/editable_content'
 import { LoadingFeedback } from '../utility/loading_feedback'
+import { IconBtn } from '../buttons/icon_btn'
 
 export const NoteDetailsPage = (props) => {
 
 	const
+    handleBackBtnClick = () => history.back(),
+    backBtn = <IconBtn
+              icon="arrow_back"
+              handleClick={handleBackBtnClick}
+            />
+    ,
 	  pageTitle = props.subsReady? <PageTitle title={props.note.title} /> : null,
 	  noteContent = () => props.subsReady? <EditableContent contentValue={props.note.content}  handleUpdates={props.handleUpdateNote} field={"content"} {...props} /> : <LoadingFeedback />
 
   return <div id="app-container" className="l-app-full-height l-app-centered">
-           <AppHeader middleCol={pageTitle} />
+           <AppHeader leftCol={backBtn} middleCol={pageTitle} />
            <div id="main-content">
            {noteContent()}
            </div>
