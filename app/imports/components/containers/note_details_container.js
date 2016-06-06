32f2433fd42971d889c45e4fbfcc31f8bb74d104
Anders Ramsay <andersr@gmail.com>
2016-06-06 11:16:49 -0400

    note details container

---
+++ app/imports/components/containers/note_details_container.js
@@ -0,0 +1,21 @@
+import { createContainer } from 'meteor/react-meteor-data'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { Meteor } from 'meteor/meteor'
+import { Note } from '../../collections/notes'
+import { App } from '../app'
+
+export default createContainer(
+	() => {
+		
+		const
+		  noteId = FlowRouter.getParam('_id'),
+		  sub = Meteor.subscribe('note.details', noteId),
+			note = sub.ready()? Note.findOne({_id: noteId }) : {}
+
+	  return {
+	  	note,
+	  	subsReady: sub.ready()
+	  }
+  },
+  App
+)
