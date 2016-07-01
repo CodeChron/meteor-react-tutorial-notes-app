d6934c6a8293e3316a963000a6b266c95d7771f1
Anders Ramsay <andersr@gmail.com>
2016-07-01 12:32:30 -0400

    reactively update signedInMsg

--- app/imports/components/pages/homepage.jsx
+++ app/imports/components/pages/homepage.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import { AppHeader } from '../layouts/app_header'
 import { PageTitle } from '../content/page_title'
-import { NotesListContainer }  from '/imports/containers/notes_list_container'
-
-//This is an example of a basic React component. Yes, it is actually just a normal (ES6) JS function.  However, React is rewriting the parts that look like HTML using React.createElement()  It is recommended that you write most of your components like this, and only use React.Component in cases where your component has state. (You should try to keep your components 'dumb' so only a minority of your components should have state.)
 export const Homepage = props => {
 
-  //Here we are instantiating a PageTitle component and passing in a value for the 'title' prop.  I recommend heading over to /content/page_title to see how this corresponds to the component itself.
 	const pageTitle = <PageTitle title={"A Notes App"} />
 
-  //This what this component actually renders (or 'returns') in the UI
-  //Here we are passing in the <PageTitle /> component into the AppHeader.  Below that, we are rendering the <NotesListContainer />
-  //NEXT: Head over to the /imports/containers/notes_list_container.jsx
+  const signedInMsg = props.currentUser? "You are signed in." : "You are not signed in."
+
   return <div id="app-container">
            <AppHeader middleCol={pageTitle} rightCol={props.loginButtons} />
            <div id="main-content">
-             <NotesListContainer {...props} />
+             {signedInMsg}
            </div>
          </div>	
 }
