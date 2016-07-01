15c67272080742d2d44bde178b716c93d0f3b8f4
Anders Ramsay <andersr@gmail.com>
2016-07-01 14:19:07 -0400

    rm comments

--- app/imports/components/lists/list.jsx
+++ app/imports/components/lists/list.jsx
@@ -2,19 +2,14 @@ import React from 'react'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { DeleteBtn }  from '../buttons/delete_btn'
 
-//This is a somewhat more advanced but still 'stateless' component
 export const List = props => {
 
-  //Here is an example of having a collection of optional component features
-  //Each feature can be turned on by setting its prop to true (see 'List.propTypes' below.)  We are "turning on" these features in the NotesListContainer.
-  //Doing so, will call the function corresponding to the feature name, pass in any props needed, and return the corresponding content block.
 	const listFeatures = {
   	itemTitle: item => <span className="flex-main-content">{item.title}</span>,
   	linkItem: (item, linkRoute) => <a href={FlowRouter.path(linkRoute , { _id: item._id })} className="flex-main-content">{item.title}</a>,
   	deleteBtn: (item, handleDelete) => <DeleteBtn handleDelete={handleDelete} size={"btn-small"} itemToDelete={item} />
 	}
   
-  //This is where we generate our list, using the map() method, which returns an array that will be assigned to 'displayList'  Here, we are using the ternary operator to check if a given optional feature, eg 'linkItem' has been turned on.  See the top-level container component, in which turn these features on by returning true for the corresponding props.
   const
     displayList = props.collection.map((item) => 
     	<li key={item._id} className="flex-vertical-middle">
@@ -27,27 +22,19 @@ export const List = props => {
         {props.addItem? props.addItem : null }
         {displayList}
       </ul>
-
 }
 
-//This is where we set the props which this component supports.  One can think of this as a mini api for this component.  A developer can quickly look at this and see how to interface with the compoenent.
 List.propTypes = {
 	collection:   React.PropTypes.array.isRequired,
-  itemCount: React.PropTypes.number,
 	addItem:      React.PropTypes.object,
 	linkItem:     React.PropTypes.bool,
 	deleteItem:   React.PropTypes.bool,
-  linkRoute:    React.PropTypes.string,
-  emptyListMsg: React.PropTypes.string
+  linkRoute:    React.PropTypes.string
 }
 
-//This is where we set default values for props.  Props for which a default value is not set will default to null.  However, for any props that are not required you shuold set a default value.
 List.defaultProps = {
 	addItem:      null,
 	linkItem:     false,
 	linkRoute:    null,
-	deleteItem:   false,
-  emptyListMsg: "No items"
+	deleteItem:   false
 }
-
-//NEXT: head over to /imports/components/forms/single_field_submit.jsx
--- app/imports/containers/notes_list_container.jsx
+++ app/imports/containers/notes_list_container.jsx
@@ -1,11 +1,7 @@
 import React from 'react' 
 import { createContainer } from 'meteor/react-meteor-data'
 import { Count } from 'meteor/tmeasday:publish-counts'
-
-//Here, we are importing our MongoDb collection and a data schema.  Head over there to check that out and then come back here: /imports/collections/note.js and then come back here. 
 import { Note } from '/imports/collections/notes'
-
-//Here, we are importing components we will be using inside the container.
 import { SingleFieldSubmit }  from '/imports/components/forms/single_field_submit'
 import { List }  from '/imports/components/lists/list'
 import { LoadingWrapper }  from '/imports/components/utility/loading_wrapper'
@@ -13,19 +9,14 @@ import { LoadingWrapper }  from '/imports/components/utility/loading_wrapper'
 export const NotesListContainer = createContainer(props => {
 		
 		const
-			//Here, we are creating a subcription to the 'notes.list' publication.  See /imports/collections/server/publications.js - the 'server' directory is significant. It is one of Meteor's special directories, and by naming that way we ensure that code inside that directory will only run on the server.
 			sub = Meteor.subscribe('notes.list')
 		,
-		  //Here we are defining a boolean for checking if our subscription is ready (ie we have connected to the server publication and are receiving our real-time subscription stream)
 		  subReady = sub.ready()
 		,
-			//Here, we are defining our collection based on if the above subscription is ready or not. By ready, we mean that a connection has been established to the reactive data source.  Until it is ready, we are simply returning an empty array. The reason we need to append the fetch() method to the end is because we want array and not a cursor. Fetch() handles that for us.
 	    notes = subReady? Note.find({}, { sort: { updatedAt: -1} }).fetch() : []
 	  ,
-
 	    notesCount = subReady? Counts.get('note_count') : null
 	  ,
-	    //Here, we are handling db operations relating to notes.  This is an interface to a Meteor db operation.  This file will run both on the client and the server side, which allows for the db operation to run 'optimistically' on the client side, but is retracted if it fails on the server side.  What this means, in terms of user experience, is that when, for example, you enter some data, you see the update on your screen immediately, because the app optimistically assumes your change will be approved on the server side.  In the unlikely event that you are doing something unapproved, the change would be reverted.
 		  handleCreate = title => {
 		    Meteor.call('note.create', title, (err, result) => {
           if (err) {
@@ -42,33 +33,29 @@ export const NotesListContainer = createContainer(props => {
         })
 		  }
 		, 
-      //Here we are setting props to be used in the List component below depending on if a user is signed in or not, ie if currentUser is truthy or not.
       addItemPlaceholder = notesCount === 0? "Type something and hit return to add a note..." : "New Note..."
-    , addItem = props.currentUser? <SingleFieldSubmit handleSubmit={handleCreate} placeholder={addItemPlaceholder} /> : null
-    , deleteItem = props.currentUser? true : false
-
-    , linkItem = props.currentUser? true : false
-
-    , displayEmptyListMsg = () => {
+    , 
+      addItem = props.currentUser? <SingleFieldSubmit handleSubmit={handleCreate} placeholder={addItemPlaceholder} /> : null
+    , 
+      deleteItem = props.currentUser? true : false
+    , 
+      linkItem = props.currentUser? true : false
+    , 
+      displayEmptyListMsg = () => {
         const
            msg =  "Please sign in or register to add notes."
         ,  msgBlock = <li className="helper-full-width help-text">{msg}</li>
         ,  displayMsg = notesCount === 0 && props.currentUser === null
 
         return displayMsg? msgBlock : null
-    }
-    , list = <List collection={notes} addItem={addItem} linkRoute={"noteDetails"} deleteItem={deleteItem} linkItem={linkItem} handleDelete={handleDelete} itemCount={notesCount} displayEmptyListMsg={displayEmptyListMsg} />
-
-
-		//This is where we return, or make available data to child components. In this case, we are returning a list component with all the props set.
-		//The single token object syntax (eg 'subReady') is a feature of ES6, and is shorthand for 'token:token' eg 'subReady:subReady'  
+      }
+    , 
+      list = <List collection={notes} addItem={addItem} linkRoute={"noteDetails"} deleteItem={deleteItem} linkItem={linkItem} handleDelete={handleDelete} itemCount={notesCount} displayEmptyListMsg={displayEmptyListMsg} />
+ 
 	  return {
       component: list,
       subReady
 	  }
   },
-  //We are wrapping this container around a "loading wrapper" which will display a loading animation until data is ready to be displayed, and will then display the component we are returning.
   LoadingWrapper
 )
-
-//NEXT: Header over to the /imports/components/utility/loading_wrapper.jsx
