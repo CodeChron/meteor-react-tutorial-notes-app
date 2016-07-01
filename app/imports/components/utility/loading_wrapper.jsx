94b8adddb89f838d34f9c9016e3ceb95b7e01cf7
Anders Ramsay <andersr@gmail.com>
2016-07-01 14:22:26 -0400

    add loading anim

--- app/imports/components/utility/loading_wrapper.jsx
+++ app/imports/components/utility/loading_wrapper.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Loading } from './loading'
 
-
 //this is a very simple yet useful component.  It accepts a loading animation and a component and a subsReady boolean.  The loading animation displays until subscriptions are ready.  That's it.
 //If you wanted to make this component a little more fancy, you could support different animations, such as a tiny animation for smaller elements.
 export const LoadingWrapper = props => props.subReady? props.component : <Loading />
