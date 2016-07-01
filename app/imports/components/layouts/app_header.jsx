67ec7b419c32368a66be3ea63733e741eefe2dd7
Anders Ramsay <andersr@gmail.com>
2016-07-01 09:33:36 -0400

    left-align page title

--- app/imports/components/layouts/app_header.jsx
+++ app/imports/components/layouts/app_header.jsx
@@ -3,15 +3,13 @@ import { LoginButtons } from '../accounts/login_buttons'
 
 export const AppHeader = props => {
 
-	return  <div id="app-header" className="flex-row flex-centered flex-vertical-middle color-inverted">
-	          <div>{props.leftCol}</div>
+	return  <div id="app-header" className="flex-row flex-vertical-middle color-inverted">
 	          <div className="flex-main-content">{props.middleCol}</div>
 	          <div><LoginButtons align="right" /></div>
 	        </div>
 }
 
 AppHeader.propTypes = {
-  leftCol:   React.PropTypes.object,
   middleCol: React.PropTypes.object,
   rightCol:  React.PropTypes.object
 }
