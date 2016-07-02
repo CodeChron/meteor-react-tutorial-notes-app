967aa54c2d7795a98657413ac85c144be1e549d6
Anders Ramsay <andersr@gmail.com>
2016-07-02 11:05:42 -0400

    rm comments

--- app/imports/startup/client/index.js
+++ app/imports/startup/client/index.js
@@ -1,9 +1,4 @@
-// This is what one can think of as an 'import manifest file'.  It lists all the files we will want to import on startup to the client side. (Then, we import the files by importing *this* file - see /client/main.js)
-// The order of the imports matters. Here,for example, we are first loading our stylesheets.  Those stylesheets, in turn, will be available to the routes (or pages) that we load next. In both the imports below, we are pointing to files and loading the entire file.  It is not necessary to include the file name extension (ie. these files are actually named main.scss and routes.jsx), unless there are multiple files of the same name in the same directory.
-
 import '/imports/stylesheets/main'
 import './routes'
-
-//NEXT: Continue to /imports/stylesheets/main.scss
 
 
