24461b14584c8c5dea371613a36d61cc60671134
Anders Ramsay <andersr@gmail.com>
2016-07-04 10:22:08 -0400

    app container comments

--- app/imports/containers/app_container.js
+++ app/imports/containers/app_container.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 import { LoginButtons } from '/imports/components/accounts/login_buttons'
 
+//This is the component around which the container is wrapped
 const App = props => props.page(props)
 
 export const AppContainer = createContainer(() => {
