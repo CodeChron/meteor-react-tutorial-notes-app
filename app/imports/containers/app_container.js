e29b57c9b247d1359344398ca3dd8d81e40c5594
Anders Ramsay <andersr@gmail.com>
2016-07-04 10:16:25 -0400

    app container comment

--- app/imports/containers/app_container.js
+++ app/imports/containers/app_container.js
@@ -5,7 +5,7 @@ import { LoginButtons } from '/imports/components/accounts/login_buttons'
 const App = props => props.page(props)
 
 export const AppContainer = createContainer(() => {
-
+  //Here, we have access to Meteor data
   return {
 	  currentUser: Meteor.user(),
 	  loginButtons: <LoginButtons align="right" />
