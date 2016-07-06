212c1aebfab256e840fd39c6683e8aad0cc3a063
Anders Ramsay <andersr@gmail.com>
2016-07-06 08:22:14 -0400

    updates to homepage route

--- app/imports/startup/client/routes.jsx
+++ app/imports/startup/client/routes.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { mount } from 'react-mounter'
 
 //Our top-level component. Get the page "region" via props, and pass along props into the region.
-const App = props => props.page(props)
+const App = props => props.page
 
 //A page component.  We pass in props from the top-level component.  However, there is currently nothing to pass in.  We can fix that by wrapping the top-level component in a (data) container.
-const Homepage = props => <div>The Homepage</div>
+const Homepage = () => <div>The Homepage</div>
 
 FlowRouter.route('/', {
   name: 'homepage',
@@ -14,7 +14,7 @@ FlowRouter.route('/', {
 
     //Props are passed into the page using the spread operator
     mount(App, {    
-      page: props => <Homepage {...props} />
+      page: <Homepage />
     })
   }
 })
