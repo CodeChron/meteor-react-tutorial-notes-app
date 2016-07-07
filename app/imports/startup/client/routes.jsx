ba7b5128537a1af4589f976069390c1219961897
Anders Ramsay <andersr@gmail.com>
2016-07-07 11:11:19 -0400

    add AppContainer to example

--- app/imports/startup/client/routes.jsx
+++ app/imports/startup/client/routes.jsx
@@ -1,20 +1,28 @@
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import React from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
 import { mount } from 'react-mounter'
 
-//Our top-level component. Get the page "region" via props, and pass along props into the region.
-const App = props => props.page
+//A page component. 
+const Homepage = props => <div>The Homepage. We now have access to top-level props (eg user data here.)</div>
 
-//A page component.  We pass in props from the top-level component.  However, there is currently nothing to pass in.  We can fix that by wrapping the top-level component in a (data) container.
-const Homepage = () => <div>The Homepage</div>
+//A "pass-thru" app component
+const App = props => props.page(props)
 
+//Our top-level app container
+const AppContainer = createContainer(() => {
+  return {
+    reactiveData: "A reactive data source" 
+  }
+},
+App)
+
+//Putting it all together
 FlowRouter.route('/', {
   name: 'homepage',
   action() {
-
-    //Props are passed into the page using the spread operator
-    mount(App, {    
-      page: <Homepage />
+    mount(AppContainer, {    
+      page: props => <Homepage {...props} />
     })
   }
 })
